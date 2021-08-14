dfabe22a2772158fa4cf834e61c2e6553f868e03
FarahJamal <farahsarese@gmail.com>
2021-08-14 16:08:31 +0300

    add base structure to start server-side work

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const getEvents = require("./Modules/Events");
+const getMovies=require('./Modules/Movies');
+const getRestaurants =require('./Modules/Restaurant');
+const getActivity=require('./Modules/Activities')
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+
+app.get('/events',getEvents);
+app.get('/movies',getMovies);
+
+app.get('/restaurants',getRestaurants);
+app.get('/activity',getActivity);
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
